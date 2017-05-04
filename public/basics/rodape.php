2ebfbe1832796f5b71f1164ae7e29a950f5f1729
Kelver Macahdo <kelver_kmv@hotmail.com>
2017-05-04 00:04:40 -0400

    deploy heroku

---
+++ public/basics/rodape.php
@@ -0,0 +1,70 @@
+    <!-- footer start (Add "dark" class to #footer in order to enable dark footer) -->
+    <!-- ================ -->
+    <footer id="footer" class="clearfix dark">
+
+        <!-- .footer start -->
+        <!-- ================ -->
+        <div class="footer">
+            <div class="container">
+                <div class="footer-inner">
+                    <div class="row">
+                        <div class="col-md-6 col-md-offset-3">
+                            <div class="footer-content text-center padding-ver-clear">
+                                <div class="logo-footer"><img id="logo-footer" class="center-block" src="images/logo_light_blue.png" alt=""></div>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus illo vel dolorum soluta consectetur doloribus sit. Delectus non tenetur odit dicta vitae debitis suscipit doloribus. Lorem ipsum dolor sit amet, illo vel dolorum soluta consectetur.</p>
+                                <ul class="list-inline mb-20">
+                                    <li><a href="tel:+00 1234567890" class="link-dark"><i class="text-default fa fa-phone pl-10 pr-5"></i>+00 1234567890</a></li>
+                                    <li><a href="mailto:info@theproject.com" class="link-dark"><i class="text-default fa fa-envelope-o pl-10 pr-5"></i>contato@plantasbrasil.com</a></li>
+                                </ul>
+                                <ul class="social-links circle animated-effect-1 margin-clear">
+                                    <li class="facebook"><a target="_blank" href="http://www.facebook.com/"><i class="fa fa-facebook"></i></a></li>
+                                    <li class="googleplus"><a target="_blank" href="http://plus.google.com/"><i class="fa fa-google-plus"></i></a></li>
+                                    <li class="linkedin"><a target="_blank" href="http://www.linkedin.com/"><i class="fa fa-linkedin"></i></a></li>
+                                </ul>
+                                <div class="separator"></div>
+                                <p class="text-center margin-clear">Copyright © <?=date('Y')?> Plantas do Brasil por <a target="_blank" href="http://gweb.com.br/">GWeb</a>. Todos os direitos reservados.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- .footer end -->
+
+    </footer>
+    <!-- footer end -->
+
+</div>
+<!-- JavaScript files placed at the end of the document so the pages load faster -->
+<!-- ================================================== -->
+<!-- Jquery and Bootstap core js files -->
+<script type="text/javascript" src="assets/plugins/jquery.min.js"></script>
+<script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script>
+<!-- Modernizr javascript -->
+<script type="text/javascript" src="assets/plugins/modernizr.js"></script>
+<!-- jQuery Revolution Slider  -->
+<script type="text/javascript" src="assets/plugins/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+<script type="text/javascript" src="assets/plugins/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+<!-- Isotope javascript -->
+<script type="text/javascript" src="assets/plugins/isotope/isotope.pkgd.min.js"></script>
+<!-- Magnific Popup javascript -->
+<script type="text/javascript" src="assets/plugins/magnific-popup/jquery.magnific-popup.min.js"></script>
+<!-- Appear javascript -->
+<script type="text/javascript" src="assets/plugins/waypoints/jquery.waypoints.min.js"></script>
+<!-- Count To javascript -->
+<script type="text/javascript" src="assets/plugins/jquery.countTo.js"></script>
+<!-- Parallax javascript -->
+<script src="assets/plugins/jquery.parallax-1.1.3.js"></script>
+<!-- Contact form -->
+<script src="assets/plugins/jquery.validate.js"></script>
+<!-- Background Video -->
+<script src="assets/plugins/vide/jquery.vide.js"></script>
+<!-- Owl carousel javascript -->
+<script type="text/javascript" src="assets/plugins/owl-carousel/owl.carousel.js"></script>
+<!-- SmoothScroll javascript -->
+<script type="text/javascript" src="assets/plugins/jquery.browser.js"></script>
+<script type="text/javascript" src="assets/plugins/SmoothScroll.js"></script>
+<!-- Initialization of Plugins -->
+<script type="text/javascript" src="assets/js/template.js"></script>
+<!-- Custom Scripts -->
+<script type="text/javascript" src="assets/js/custom.js"></script>
